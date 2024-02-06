25c0aec4900a58e79aed81446036d52e55f3212d
liviakmm10 <liviakmm10@gmail.com>
2024-02-06 11:07:57 -0300

    Update: Eliminei linhas de código desnecessárias

--- js/script.js
+++ js/script.js
@@ -3,25 +3,16 @@ let sorvetes = [
     {nome:'Prestígio' , sabor1:'chocolate', sabor2:'coco'},
     {nome:'Flocos' , sabor1:'baunilha', sabor2:'chocolate'},
 ]
-// const container = document.createElement("div")
-// container.classList("container")//no elemento container coloquei uma classe container
-
-// let listaSorvetes = document.createElement("ul")
-
 const ul = document.querySelector("#listaSorvetes")
-
-// container.appendChild(listaSorvetes)
 
 
 sorvetes.forEach(sorvete => {
-    // console.log(sorvete.nome)
 
     const produto = document.createElement("li") 
-    // ul.appendChild(sorvete.nome)
-
     ul.appendChild(produto)
-
     produto.innerHTML = sorvete.nome
 
-
 });
+
+
+
