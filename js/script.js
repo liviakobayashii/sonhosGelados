d752ad74b4d6764bdd3ffbb167beb6117259a8fe
liviakmm10 <liviakmm10@gmail.com>
2024-02-11 23:48:17 -0300

    feat: na páginna inicial criei a funcionalidade de filtrar sorvetes pelo nome e criei uma pagina de adicionar os sorvetes no cardápio

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
-let sorvetes = [
-    {nome:'Sensação' , sabor1:'morango', sabor2:'chocolate'},
-    {nome:'Prestígio' , sabor1:'chocolate', sabor2:'coco'},
-    {nome:'Flocos' , sabor1:'baunilha', sabor2:'chocolate'},
-]
+let sorvetes = JSON.parse(localStorage.getItem('@sorveteriaOnline:sorvetes')) || []
+
+const formAdicionar = document.querySelector("#formAdicionar")
+const inputNome = document.querySelector("#inputNome")
+
+const formFiltrar = document.querySelector("#formFiltrar")
+const inputFiltrar = document.querySelector("#inputFiltrar")
+
 const ul = document.querySelector("#listaSorvetes")
 
-
 sorvetes.forEach(sorvete => {
-
     const produto = document.createElement("li") 
     ul.appendChild(produto)
     produto.innerHTML = sorvete.nome
 
 });
 
+//filtrar sorvetes
 
+formFiltrar.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    
+    let valor = inputFiltrar.value
+    let saborSelecionado = sorvetes.filter((sorvete) => sorvete.nome == valor)
+    if(saborSelecionado.length != 0){
+        console.log(saborSelecionado)
+    }else{
+        console.log("Sorvete não encontrado")
+    }
+
+});
 
