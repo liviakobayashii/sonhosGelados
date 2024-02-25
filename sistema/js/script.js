d5299875aeecfb576f391bda81aae73e9b7219fb
liviakmm10 <liviakmm10@gmail.com>
2024-02-25 17:00:41 -0300

    feat: criei a funçaõ de filtrar os sorvetes pelo sabor

--- sistema/js/script.js
+++ sistema/js/script.js
@@ -1,5 +1,7 @@
 let sorvetes = JSON.parse(localStorage.getItem('@sorveteriaOnline:sorvetes')) || []
 
+const sorvetesAdicionados = document.querySelector(".sorvetesAdicionados")
+
 // const formAdicionar = document.querySelector("#formAdicionar")
 const inputNome = document.querySelector("#inputNome")
 
@@ -9,47 +11,58 @@ const inputFiltrar = document.querySelector("#inputFiltrar")
 const ul = document.querySelector("#listaSorvetes")
 localStorage.setItem('@sorveteriaOnline:sorvetes', JSON.stringify(sorvetes))
 
+mostrarSorveteNaPagina(sorvetes)
 
 //filtrar sorvetes
 
-// formFiltrar.addEventListener("submit", (e) =>{
-//     e.preventDefault()
+formFiltrar.addEventListener("submit", (e) =>{
+    e.preventDefault()
     
-//     let valor = inputFiltrar.value
-//     let saborSelecionado = sorvetes.filter((sorvete) => sorvete.nome == valor)
-//     if(saborSelecionado.length != 0){
-//         console.log(saborSelecionado)
-//     }else{
-//         console.log("Sorvete não encontrado")
-//     }
+    let valor = inputFiltrar.value
 
-// });
+    const saborSelecionado = sorvetes.filter((sorvete) => sorvete.nome == valor)
+
+    if(saborSelecionado.length != 0){
+        mostrarSorveteNaPagina(saborSelecionado)
+    }else{
+        mostrarSorveteNaPagina(sorvetes)
+        
+        alert("Sorvete não encontrado")
+    }
+
+});
+
+
+function mostrarSorveteNaPagina(arraySorvetes) {
+    sorvetesAdicionados.innerHTML = ''
+
+    arraySorvetes.forEach((item) => {
+
+        //mostrar os sorvetes adicionados
+        const card = document.createElement("section")
+        card.classList.add("card")
+    
+        const nomeSorvete = document.createElement("p")
+        nomeSorvete.textContent =  item.nome
+        nomeSorvete.classList.add("nome")
+    
+        const preco = item.preco || '-'
+    
+        const precoSorvete = document.createElement("p")
+        precoSorvete.textContent = "Preço: R$ " + preco
+        precoSorvete.classList.add("preco")
+    
+        card.appendChild(nomeSorvete)
+        card.appendChild(precoSorvete)
+    
+        //adiciona o card à section sorvetesAdicionados
+        sorvetesAdicionados.appendChild(card)
+    });
+}
 
 
 //adicionar sorvetes
 
-const  sorvetesAdicionados = document.querySelector(".sorvetesAdicionados")
 
-sorvetes.forEach((sorvete) => { //mostrar os sorvetes adicionados
-    const card = document.createElement("section")
-    card.classList.add("card")
-
-    const nomeSorvete = document.createElement("p")
-    nomeSorvete.textContent =  sorvete.nome
-    nomeSorvete.classList.add("nome")
-
-    const preco = sorvete.preco || '-'
-
-    const precoSorvete = document.createElement("p")
-    precoSorvete.textContent = "Preço: R$ " + preco
-    precoSorvete.classList.add("preco")
-
-    card.appendChild(nomeSorvete)
-    card.appendChild(precoSorvete)
-
-    //adiciona o card à section sorvetesAdicionados
-    sorvetesAdicionados.appendChild(card)
-
-});
 
 
