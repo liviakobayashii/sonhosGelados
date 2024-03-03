8798bad85b9da81e05ff838fa21360c536d557fc
liviakmm10 <liviakmm10@gmail.com>
2024-03-03 12:29:46 -0300

    update: otimizei o código

--- sistema/js/carrinho.js
+++ sistema/js/carrinho.js
@@ -1,10 +1,2 @@
-//função para adicionar item no carrinho
 
-function addCarrinho(idProduto){
-    let sorvetes = JSON.parse(localStorage.getItem('@sorveteriaOnline:sorvetes')) || []
-    const produto= sorvetes.find(item =>item.id ===idProduto)
-    if(produto){
-        sorvetes.push(produto)
-        localStorage.setItem('@sorveteriaOnline:sorvetes', JSON.stringify(sorvetes))
-    }
-}
+
