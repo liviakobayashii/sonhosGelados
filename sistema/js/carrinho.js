1337959b28324af82a7124d81471faa2355c42b0
liviakmm10 <liviakmm10@gmail.com>
2024-03-10 23:33:17 -0300

    fix: deixei o sistema de carrinho funcional

--- sistema/js/carrinho.js
+++ sistema/js/carrinho.js
@@ -1,47 +1,31 @@
-const tabela = document.querySelector("#tabela")
-
 const getCarrinho = () =>JSON.parse(localStorage.getItem('@sonhosGelados:carrinho')) || []
 const setCarrinho = dadosCarrinho => localStorage.setItem('@sonhosGelados:carrinho', JSON.stringify(dadosCarrinho))
 
-// const carrinho = document.querySelector("#carrinho")
+const real = Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' })
+
+const tabela = document.querySelector("#tabela")
 const buttonCarrinho = document.querySelector(".buttonCarrinho")
+const carrinho = getCarrinho()
 
-// buttonCarrinho.onclick = function(){}
+carrinho.forEach(item => {
 
-const addCarrinho = (id) => {
-    const carrinho = getCarrinho()
 
-    if(carrinho.length > 0){
-        let modificado = false
-        
-        carrinho.forEach(item => {
-            if(item.id === id){
-                item.qtd ++
-                modificado = true
-            }
-        });
-        !modificado && carrinho.push({id:id, qtd:1})
-    }
-    else{
-        carrinho.push({id:id, qtd:1})
-    }
-    setCarrinho(carrinho)
-
-    carrinho.forEach(item => {
-        const tr = document.createElement("tr")
-        const tdNome = document.createElement("td")
-        const tdQtd = document.createElement("td")
-        tr.appendChild(tdNome)
-        tr.appendChild(tdQtd)
+    const tr = document.createElement("tr")
+    const tdNome = document.createElement("td")
+    const tdQtd = document.createElement("td")
+    const tdPreco = document.createElement("td")
     
-        tdNome.textContent = item.nome
-        tdQtd.textContent = item.qtd
+    tr.appendChild(tdNome)
+    tr.appendChild(tdQtd)
+    tr.appendChild(tdPreco)
 
-        tabela.appendChild(th)
-    });
+    tdNome.textContent = item.nome
+    tdQtd.textContent = item.qtd
+    tdPreco.innerHTML = `${real.format(item.preco * item.qtd)} <small>(${item.qtd}x ${real.format(item.preco)})</small> ` 
 
+    tabela.appendChild(tr)
+});
 
-}
 // buttonCarrinho.addEventListener('click', (e) => {
 //     e.preventDefault()
 
