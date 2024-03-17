2b4f22f554e398103e8399f0adcbadaf04207579
liviakmm10 <liviakmm10@gmail.com>
2024-03-17 13:16:37 -0300

    update: descomentei o codigo do toastify e de remover item do carrinho

--- sistema/controller/remCarrinho.js
+++ sistema/controller/remCarrinho.js
@@ -1,26 +1,33 @@
 const remCarrinho = (id) => {
+    const carrinho = getCarrinho()
+
     carrinho.forEach(item => {
-        // if(item.qtd > 1){
-        //     let qtd = Number(item.qtd)
-        //     qtd = qtd -1
-        // }else{
-            let novoCarrinho = carrinho.filter(item =>item.id !== id)
+        if(item.id === id){
+            const novoCarrinho = carrinho.filter(item => item.id !== id)
             setCarrinho(novoCarrinho)
+        }
+        // if( item.qtd > 1){
+        //     let qtd = Number(item.qtd)
+        //     qtd -=1 
+        // }else if(){
+        //     let novoCarrinho = carrinho.filter(item =>item.id !== id)
+        //     setCarrinho(novoCarrinho)
         // }
     });
-location.reload()
 
-// Toastify({
-//   text: "Produto removido do carrinho com sucesso!",
-//   duration: 3000,
-//   close: true,
-//   gravity: "top", // `top` or `bottom`
-//   position: "right", // `left`, `center` or `right`
-//   stopOnFocus: true, // Prevents dismissing of toast on hover
-//   style: {
-//     background: "#97fc97",
-//     color:"#000000",
-//     border: '1px solid green'
-//   },
-// }).showToast();
+    mostrarCarrinho()
+
+Toastify({
+  text: "Produto removido do carrinho com sucesso!",
+  duration: 3000,
+  close: true,
+  gravity: "top", // `top` or `bottom`
+  position: "right", // `left`, `center` or `right`
+  stopOnFocus: true, // Prevents dismissing of toast on hover
+  style: {
+    background: "#ffb4b4",
+    color:"#000000",
+    border: '1px solid red'
+  },
+}).showToast();
 }
