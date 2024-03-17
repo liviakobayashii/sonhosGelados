d4b9dfc95255e75b9982c3c1ebf8d884f0fd9585
liviakmm10 <liviakmm10@gmail.com>
2024-03-17 13:17:42 -0300

    fix: corrigi o codigo para salvar o item modificado no localStorage

--- sistema/view/carrinho.js
+++ sistema/view/carrinho.js
@@ -5,50 +5,59 @@ const real = Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' })
 const tabela = document.querySelector("#tabela")
 const buttonCarrinho = document.querySelector(".buttonCarrinho")
 
-let carrinho = getCarrinho()
+const mostrarCarrinho = () => {
 
-let mostrarNaPagina = `
-    <tr>
-        <th>Nome</th>
-        <th>Quantidade</th>
-        <th>Preço</th>
-        <th>Deletar</th>
-    </tr>
-` 
-if (carrinho.length > 0) {
-    let soma =0
+    const carrinho = getCarrinho()
 
-    carrinho.forEach(item => {
-        mostrarNaPagina += `
-            <tr class="trProdutos">
-                <td>${item.nome}</td>
-                <td>${item.qtd}</td>
-                <td>${real.format(item.preco * item.qtd)} 
-                    <small>
-                        (${item.qtd}x ${real.format(item.preco)})
-                    </small>
-                </td>
-                <td>
-                    <button class="material-symbols-outlined" onclick=" remCarrinho(${item.id})">
-                        delete
-                    </button>
-                </td>
-            </tr>  
-        `
-        soma += item.qtd * item.preco
-    })
-    tabela.innerHTML = mostrarNaPagina
-
-    const trTotal = document.createElement("tr")
-    trTotal.innerHTML = 
-        `<td class="total" colspan="4" style="font-weight: bold;">Valor total: ${real.format(soma)}</td>`
-    tabela.appendChild(trTotal)
-
-} else{
-    mostrarNaPagina += `
+    let mostrarNaPagina = `
         <tr>
-            <td class="carrinhoVazio" colspan="4">Carrinho Vazio!</td>
-        </tr>`
-    tabela.innerHTML = mostrarNaPagina
+            <th>Nome</th>
+            <th>Quantidade</th>
+            <th>Preço</th>
+            <th>Deletar</th>
+        </tr>
+    `
+
+    if (carrinho.length > 0) {
+        let soma = 0
+
+        carrinho.forEach(item => {
+            mostrarNaPagina += `
+                <tr class="trProdutos">
+                    <td>${item.nome}</td>
+                    <td>${item.qtd}</td>
+                    <td>${real.format(item.preco * item.qtd)} 
+                        <small>
+                            (${item.qtd}x ${real.format(item.preco)})
+                        </small>
+                    </td>
+                    <td>
+                        <button class="material-symbols-outlined" onclick=" remCarrinho(${item.id})">
+                            delete
+                        </button>
+                    </td>
+                </tr>  
+            `
+            soma += item.qtd * item.preco
+        })
+
+        tabela.innerHTML = mostrarNaPagina
+
+        const trTotal = document.createElement("tr")
+
+        trTotal.innerHTML = 
+            `<td class="total" colspan="4" style="font-weight: bold;">Valor total: ${real.format(soma)}</td>`
+        tabela.appendChild(trTotal)
+
+    } else{
+        mostrarNaPagina += `
+            <tr>
+                <td class="carrinhoVazio" colspan="4">Carrinho Vazio!</td>
+            </tr>`
+        
+        tabela.innerHTML = mostrarNaPagina
+    }
+
 }
 
+mostrarCarrinho()
