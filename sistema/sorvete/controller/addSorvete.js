b2fdcaddf2303924a4f1e01ca09d1fa547099da2
liviakmm10 <liviakmm10@gmail.com>
2024-03-31 11:30:02 -0300

    feat: criação da funcionalidade de escolher o tipo de produto a ser adicionado

--- sistema/sorvete/controller/addSorvete.js
+++ sistema/sorvete/controller/addSorvete.js
@@ -3,6 +3,7 @@ const formAdicionar = document.querySelector("#formAdicionar")
 const inputNome = document.querySelector("#inputNome")
 const inputPreco = document.querySelector('#inputPreco')
 const inputImg = document.querySelector('#inputImg')
+const tipoDoProduto = document.querySelector("#tipo")
 const  sorvetesAdicionados = document.querySelector(".sorvetesAdicionados")
 
 formAdicionar.addEventListener("submit", (e) => {
@@ -11,19 +12,36 @@ formAdicionar.addEventListener("submit", (e) => {
     const valorInputNome = inputNome.value
     const valorInputPreco = inputPreco.value
     const valorInputImg = inputImg.value
+    const valorTipoDoProduto = tipoDoProduto.value
 
     let sorvetes = JSON.parse(localStorage.getItem('@sonhosGelados:sorvetes')) || []
+    let coberturas = JSON.parse(localStorage.getItem('@sonhosGelados:coberturas')) || []
+    let casquinhas = JSON.parse(localStorage.getItem('@sonhosGelados:casquinhas')) || []
+
 
     let id = 1
     sorvetes.forEach(item => {
       id += 1
     });
 
-    if(valorInputNome && valorInputPreco && valorInputImg){
-      sorvetes.push({id:id, nome:valorInputNome, preco: valorInputPreco, img: valorInputImg})
+    if(valorInputNome && valorInputPreco && valorInputImg && valorTipoDoProduto){
 
-      localStorage.setItem('@sonhosGelados:sorvetes', JSON.stringify(sorvetes))
-   
+      switch(valorTipoDoProduto){
+        case "sorvete": 
+          sorvetes.push({id:id, nome:valorInputNome, preco: valorInputPreco, img: valorInputImg})
+          localStorage.setItem('@sonhosGelados:sorvetes', JSON.stringify(sorvetes))
+          break
+        case "cobertura":
+          coberturas.push({id:id, nome:valorInputNome, preco: valorInputPreco, img: valorInputImg})
+          localStorage.setItem('@sonhosGelados:coberturas', JSON.stringify(coberturas))
+          break
+        case "casquinha":
+          casquinhas.push({id:id, nome:valorInputNome, preco: valorInputPreco, img: valorInputImg})
+          localStorage.setItem('@sonhosGelados:casquinhas', JSON.stringify(casquinhas))
+          break
+      }
+
+      console.log(coberturas)
       Toastify({
           text: "Produto adicionado ao cardápio com sucesso!",
           duration: 3000,
@@ -58,4 +76,5 @@ formAdicionar.addEventListener("submit", (e) => {
       inputNome.value = ""
       inputPreco.value = ""
       inputImg.value = ""
+      tipoDoProduto.value = ""
 })
