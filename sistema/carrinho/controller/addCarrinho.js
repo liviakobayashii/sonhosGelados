5dc3484af2f132eb32f1a7b72aa2c336a04a14c7
liviakmm10 <liviakmm10@gmail.com>
2024-04-10 00:11:25 -0300

    feat: criação da funcionalidade de adicionar o sorvete personalizado no carrinho(em andamento)

--- sistema/carrinho/controller/addCarrinho.js
+++ sistema/carrinho/controller/addCarrinho.js
@@ -1,23 +1,68 @@
 const getCarrinho = () =>JSON.parse(localStorage.getItem('@sonhosGelados:carrinho')) || []
 const setCarrinho = dadosCarrinho => localStorage.setItem('@sonhosGelados:carrinho', JSON.stringify(dadosCarrinho))
+// const getSorvetePersonalizado = () => JSON.parse(localStorage.getItem('@sonhosGelados:sorvetePersonalizado')) || personalizacaoPadrao
+
 
 const addCarrinho = (id, nome, preco) => {
     const carrinho = getCarrinho()
-    if(carrinho.length > 0){
-        let modificado = false
-        
+
+    if (existeUmSorveteComOMesmoID) {
         carrinho.forEach(item => {
-            if(item.id === id){
-                item.qtd ++
-                modificado = true
+            if (item.id === id) {
+                item.qtd += 1
             }
-        });
-        !modificado && carrinho.push({id:id, nome:nome, qtd:1, preco:preco})
+        })
+    } else {
+        carrinho.push({ id, nome, qtd:1, preco, descricao:null })//quando nome: nome
     }
-    else{
-        carrinho.push({id:id, nome:nome, qtd:1, preco:preco})
-    }
+
     setCarrinho(carrinho)
+
+    Toastify({
+        text: "Produto adicionado ao carrinho com sucesso!",
+        duration: 3000,
+        close: true,
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "#97fc97",
+          color:"#000000",
+          border: '1px solid green'
+        },
+      }).showToast();
+
+}
+
+
+const addCarrinhoPersonalizado = () => {
+    const carrinho = getCarrinho()
+    const sorvetePersonalizado = getSorvetePersonalizado()
+
+    const { primeiroSabor, segundoSabor, cobertura, casquinha } = sorvetePersonalizado
+
+    const novoSorvete = {
+        id: [primeiroSabor.id, segundoSabor.id, cobertura.id, casquinha.id].join(''),
+        nome: 'Sorvete personalizado',
+        descricao: `${primeiroSabor.nome}, ${segundoSabor.nome}, cobertura de ${cobertura.nome} e casquinha de ${casquinha.nome}`,
+        preco: parseFloat(primeiroSabor.preco) + parseFloat(segundoSabor.preco) + parseFloat(cobertura.preco) + parseFloat(casquinha.preco)
+    }
+
+    const existeUmSorveteComOMesmoID = carrinho.some(item => item.id === novoSorvete.id)
+
+    if (existeUmSorveteComOMesmoID) {
+        carrinho.forEach(item => {
+            if (item.id === novoSorvete.id) {
+                item.qtd += 1
+            }
+        })
+    } else {
+        carrinho.push(novoSorvete)
+    }
+
+    setCarrinho(carrinho)
+
+    console.log(carrinho)
 
     Toastify({
         text: "Produto adicionado ao carrinho com sucesso!",
---
+++ sistema/personalizarSorvete/controller/addSorvete.js
@@ -0,0 +1,14 @@
+
+
+let arraySorvetePersonalizado = [] 
+
+const addSorvetePersonalizado = () => {
+
+    arraySorvetePersonalizado.push(getSorvetePersonalizado)
+
+    let idPersonalizado = 1
+    arraySorvetePersonalizado.forEach(item => {
+      idPersonalizado += 1
+    });
+
+}
